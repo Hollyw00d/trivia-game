1cc21538ed235e6f3792855844657ad09cd7981b
Matt Jennings <matt@mattjennings.net>
2019-06-18 20:54:52 -0700

    middle of creating trivia questions

---
+++ assets/js/game.js
@@ -0,0 +1,28 @@
+/*
+"Anchor Man" movie trivia quiz
+
+1. What are Ron Burgundy's first words spoken in the movie?
+a. Mmm. I look good.
+b. How now brown cow.
+c. Great Odin's raven!
+d. Hey, everyone! Come and see how good I look!
+
+2. What is Ron Burgundy's middle name?
+a. Shirley
+b. Brandie
+c. Beverly
+d. Marianne
+
+3. According to the movie narrator, what does Sinatra look like compared to Ron Burgundy in one of his fine suits?
+a. A bum
+b. A hobo
+c. A poser
+d. A wannabe
+
+4. What does the "Escupimos en su Alimento" restaurant mean in English, that Veronica and the ladies from the station visit?
+a. I Spit in Your Food
+b. I Blow my Nose in Your Food
+c. I Give you Food for Free
+d. We Spit in Your Food
+
+*/
